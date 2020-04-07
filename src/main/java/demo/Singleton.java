9cfe6f7c985be3f828196e2ecf34dc1025475d86
Matt Payne <Payne@MattPayne.org>
2020-04-06 21:10:51 -0500

    Just starting the disgusting global variable bit.

--- src/main/java/demo/Singleton.java
+++ src/main/java/demo/Singleton.java
@@ -1,4 +1,28 @@
 package demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Singleton {
+    private static Singleton singleton=null;
+
+    private List<Map<String,String>> lines;
+
+    public Singleton getSingleton() {
+        if (null==singleton) singleton = new Singleton();
+        return singleton;
+    }
+
+    public List<Map<String, String>> getLines() {
+        return lines;
+    }
+
+    public void setLines(List<Map<String, String>> lines) {
+        this.lines = lines;
+    }
+
+    private Singleton() {
+        this.lines = new ArrayList<>();
+    }
 }
